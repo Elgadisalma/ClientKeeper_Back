364ce7254e3d1fa9c9cf9f6e3402e0ef47fa2b78
Elgadisalma <elgadi.salma0@gmail.com>
2025-01-06 17:25:13 +0100

    CK-26 creer le projet

---
+++ clientKeeper/src/main/java/org/example/clientkeeper/ClientKeeperApplication.java
@@ -0,0 +1,13 @@
+package org.example.clientkeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientKeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientKeeperApplication.class, args);
+    }
+
+}
