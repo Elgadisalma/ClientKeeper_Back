6fe2df3cdc54f5fd1408f2f21d9507e306b00694
Elgadisalma <elgadi.salma0@gmail.com>
2025-01-13 14:45:04 +0100

    CK-8 valider le code pin

--- clientKeeper/src/main/java/org/example/clientkeeper/dto/TransactionDTO.java
+++ clientKeeper/src/main/java/org/example/clientkeeper/dto/TransactionDTO.java
@@ -5,11 +5,12 @@ import java.time.LocalDateTime;
 
 @Data
 public class TransactionDTO {
-    private Long receiverId;
+//    private Long receiverId;
     private String receiverName;
     private String receiverPrenom;
     private String receiverAccountNumber;
     private LocalDateTime dateTransaction;
     private double montant;
     private String motif;
+    private String securePin;
 }
