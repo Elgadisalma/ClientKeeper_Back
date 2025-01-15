56ec7b2e12fe5a6444ec3c98d196d5c6fc65408c
Elgadisalma <elgadi.salma0@gmail.com>
2025-01-15 09:12:35 +0100

    CK-6 get client by id

---
+++ clientKeeper/src/main/java/org/example/clientkeeper/controller/ClientController.java
@@ -0,0 +1,24 @@
+package org.example.clientkeeper.controller;
+
+import org.example.clientkeeper.dto.ClientDTO;
+import org.example.clientkeeper.service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/clients")
+public class ClientController {
+
+    @Autowired
+    private ClientService clientService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ClientDTO> getClientById(@PathVariable Long id) {
+        ClientDTO client = clientService.getClientById(id);
+        return ResponseEntity.ok(client);
+    }
+}
